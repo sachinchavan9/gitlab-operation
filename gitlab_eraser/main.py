9407133e4a532281bd937d9661bca64fdb806d22
sachin <sachinewx@gmail.com>
2021-02-18 20:20:41 +0530

    last commit

--- gitlab_eraser/main.py
+++ gitlab_eraser/main.py
@@ -1,11 +1,18 @@
 from userconfig import UserConfig
 import gitlab
 import os
+from prettytable import PrettyTable
+import json
+import sys
+
+import warnings
+warnings.filterwarnings("ignore")
 
 
 class Remover(UserConfig):
     def __init__(self):
         UserConfig.__init__(self)
+        self._project_id = None
         try:
             self._get_project_id()
         except KeyboardInterrupt:
@@ -21,29 +28,105 @@ class Remover(UserConfig):
     BE CAREFULL WHILE ADDING PROJECT ID
     IT WILL WIPE ALL YOUR PROJECT ISSUES
 
-    GitLab:{}
-    AccessToken:{}
+    GitLab: {}
+    AccessToken: {}
+    Config File Path: {}
     ############################################
     ############################################
-            '''.format(self.url, self.token)
+            '''.format(self.url, self.token, self._config_file_path)
         )
         try:
-            project_id = int(input('Project ID:'))
-            print('\n[-] remove issues from project id: {}'.format(project_id))
-            self._remove_issue(project_id=project_id)
+            gitman = self.gitman(
+                url=self.url, token=self.token, ssl_verify=False)
+            self._get_projects(gitman)
+            self._project_id = int(input('\nProject ID: '))
+            print(
+                '\n[-] counting issues from project id: {}'.format(self._project_id))
+            self._remove_issue(project_id=self._project_id, gitlab=gitman)
         except ValueError:
             print('\n[x] wrong input!\n')
             exit(0)
+        except Exception as ex:
+            print('\n[x] {}\n'.format(ex))
+
+    def _get_projects(self, gl):
+        projects = gl.projects.list(all=True)
+        x = PrettyTable()
+        x.field_names = ['ID', 'Project']
+        x.align["ID"] = "r"
+        x.align["Project"] = "l"
+        rows = [[project.id, project.name_with_namespace]
+                for project in projects]
+        x.add_rows(rows)
+        print(x)
 
     def gitman(self, url, token, ssl_verify=False):
         git = gitlab.Gitlab(url=url, private_token=token,
                             ssl_verify=ssl_verify)
         return git
 
-    def _remove_issue(self, project_id):
-        
-        git = self.gitman(self.url, self.token, ssl_verify=False)
-        print(project_id)
+    def _remove_issue(self, project_id, gitlab):
+        project = gitlab.projects.get(project_id)
+        issues = project.issues.list(all=True)
+
+        open_issue = [i for i in issues if i.state == 'opened']
+        oi = len(open_issue)
+        print('\n[+] Open: {}'.format(oi))
+
+        closed_issue = [i for i in issues if i.state == 'closed']
+        ci = len(closed_issue)
+
+        ai = oi+ci
+        print('[x] Closed: {}'.format(ci))
+        print('[-] All: {}'.format(ai))
+
+        if not ai:
+            print('\n[x] no issues for erase\n')
+            exit(0)
+
+        confirm = input(
+            '\nAre you sure to erase issues from GitLab project ID#{} (y/N): '.format(project_id))
+        if not confirm:
+            print('\n[x] good bye!\n')
+            exit(0)
+        elif confirm.lower() == 'y':
+            print('\n[-] erasing the project issues, plese wait...\n')
+            # -------------------------------------------------------------------------
+            # progress bar
+            current_counter = 0
+            COUNTER = 1
+
+            total_iteration = ai
+
+            sys.stdout.write("|{}|".format("-" * 100))
+            sys.stdout.flush()
+            sys.stdout.write("\b" * 101)
+            # -------------------------------------------------------------------------
+
+            for i in issues:
+                status_bar_string = "{} of {} ".format(
+                    COUNTER, total_iteration)
+                sys.stdout.write(status_bar_string)
+                sys.stdout.flush()
+
+                sys.stdout.write("\b" * len(status_bar_string))
+                # progress bar code above
+                # main code
+                project.issues.delete(i.iid)
+
+                # progress bar
+                percent = (COUNTER / total_iteration) * 100
+                if current_counter < int(percent):
+                    current_counter = int(percent)
+                    sys.stdout.write('█' * int(percent))
+                    sys.stdout.write("\b" * int(percent))
+                COUNTER += 1
+                sys.stdout.flush()
+            sys.stdout.write("\n")
+            print('\n[+] {} issues are successfully erased...!\n'.format(ai))
+        else:
+            print('\n[x] wrong input!\n')
+            exit(0)
 
 
 if __name__ == "__main__":
--- gitlab_eraser/userconfig.py
+++ gitlab_eraser/userconfig.py
@@ -11,17 +11,21 @@ class UserConfig(GitInstance):
             self.user_action()
 
     def user_action(self):
-        print('\n[===============|GitLab User Config|===============]')
-        url = input('\nEnter GitLab URL(default is https://gitlab.com/): ')
-        access_token = input('Enter AccessToken (default is False): ')
-        print('\n[+] setting git url: {}'.format(url))
-        print('[+] setting access token: {}'.format(access_token))
-        if url:
-            self._git_url = url
-        else:
-            print('[x] setting default gitlab url')
-        if access_token:
-            self._access_token = access_token
-        else:
-            print('[x] setting access token "False"')
-        self._default_config()
+        try:
+            print('\n[===============|GitLab User Config|===============]')
+            url = input('\nEnter GitLab URL(default is https://gitlab.com/): ')
+            access_token = input('Enter AccessToken (default is False): ')
+            print('\n[+] setting git url: {}'.format(url))
+            print('[+] setting access token: {}'.format(access_token))
+            if url:
+                self._git_url = url
+            else:
+                print('[x] setting default gitlab url')
+            if access_token:
+                self._access_token = access_token
+            else:
+                print('[x] setting access token "False"')
+            self._default_config()
+        except KeyboardInterrupt:
+            print('\n[x] good bye..\n')
+            exit(0)
